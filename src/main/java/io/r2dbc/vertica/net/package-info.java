42db59db47cafa48cdefba3149a5162abe95b213
Michael Gillette <me@michaelgillette.net>
2023-04-16 22:44:52 -0700

    adds VerticaConnectionMetadata
    - adjusts .editorconfig to allow for 120 characters in java files
    - adds assertj
    - adds maven-surefire-plugin
    - adds Version and VersionTest

---
+++ src/main/java/io/r2dbc/vertica/net/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Defines the network interface between the client and server.
+ *
+ * @since 0.0.1
+ */
+@NonNullApi
+package io.r2dbc.vertica.net;
+
+import reactor.util.annotation.NonNullApi;
