481c558194065e1f7ac12321e59c614e43e63121
Michael Gillette <me@michaelgillette.net>
2023-08-12 22:49:44 -0700

    defines ErrorResponse

--- src/main/java/io/r2dbc/vertica/protocol/message/backend/ErrorResponse.java
+++ src/main/java/io/r2dbc/vertica/protocol/message/backend/ErrorResponse.java
@@ -1,5 +1,69 @@
 package io.r2dbc.vertica.protocol.message.backend;
 
-public final class ErrorResponse {
+import io.netty.buffer.ByteBuf;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public final class ErrorResponse implements BackendMessage<ErrorResponse.Data> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Data decode(ByteBuf src) {
+        if (src.readableBytes() == 0) {
+            throw new IllegalArgumentException("expected non-empty content");
+        }
+
+        var error = src.readCharSequence(src.bytesBefore((byte) 0), StandardCharsets.UTF_8).toString();
+
+        src.skipBytes(1);
+
+        return new Data(error);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void encode(Data content, ByteBuf dst) {
+        dst.writeCharSequence(content.error(), StandardCharsets.UTF_8);
+        dst.writeByte(0);
+    }
+
+    public static final class Data implements BackendContent {
+
+        private final String error;
+
+        public Data(String error) {
+            this.error = error;
+        }
+
+        public String error() {
+            return error;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Data data = (Data) o;
+            return Objects.equals(error, data.error);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(error);
+        }
+
+        @Override
+        public String toString() {
+            return "ErrorResponse.Data{" +
+                    "error='" + error + '\'' +
+                    '}';
+        }
+
+    }
 
 }
---
+++ src/test/java/io/r2dbc/vertica/protocol/message/backend/ErrorResponseTest.java
@@ -0,0 +1,74 @@
+package io.r2dbc.vertica.protocol.message.backend;
+
+import io.netty.buffer.Unpooled;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
+
+class ErrorResponseTest {
+
+    private final ErrorResponse subject = new ErrorResponse();
+
+    @Nested
+    @DisplayName("decode(ByteBuf)")
+    class DecodeByteBuf {
+
+        @Test
+        void whenTooFewBytes_thenThrow() {
+            var src = Unpooled.buffer();
+
+            Assertions.assertThatThrownBy(() -> subject.decode(src))
+                .isOfAnyClassIn(IllegalArgumentException.class)
+                .hasMessage("expected non-empty content");
+        }
+
+        @ParameterizedTest
+        @ValueSource(
+            strings = { "hello", "world" }
+        )
+        void whenOk_thenReturn(String error) {
+            var src = Unpooled.buffer();
+            src.writeCharSequence(error, StandardCharsets.UTF_8);
+            src.writeByte((byte) 0);
+
+            Assertions.assertThat(subject.decode(src))
+                .isEqualTo(new ErrorResponse.Data(error));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("encode(T, ByteBuf)")
+    class EncodeTByteBuf {
+
+        @ParameterizedTest
+        @MethodSource("io.r2dbc.vertica.protocol.message.backend.ErrorResponseTest#contentArgs")
+        void whenOk_thenReturn(ErrorResponse.Data content) {
+            var dst = Unpooled.buffer();
+
+            subject.encode(content, dst);
+
+            Assertions.assertThat(dst.readCharSequence(content.error().length(), StandardCharsets.UTF_8))
+                .isEqualTo(content.error());
+
+            Assertions.assertThat(dst.readByte())
+                .isEqualTo((byte) 0);
+        }
+
+    }
+
+    public static Stream<ErrorResponse.Data> contentArgs() {
+        return Stream.of(
+            new ErrorResponse.Data("hello"),
+            new ErrorResponse.Data("world")
+        );
+    }
+
+}
