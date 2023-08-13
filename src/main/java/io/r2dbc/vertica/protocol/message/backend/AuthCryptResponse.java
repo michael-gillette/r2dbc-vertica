2a2fe485d7ed03970176160ede9fa5908b3ada1b
Michael Gillette <me@michaelgillette.net>
2023-08-12 23:26:21 -0700

    adds AuthCryptResponse

--- src/main/java/io/r2dbc/vertica/protocol/message/backend/AuthCryptResponse.java
+++ src/main/java/io/r2dbc/vertica/protocol/message/backend/AuthCryptResponse.java
@@ -1,5 +1,67 @@
 package io.r2dbc.vertica.protocol.message.backend;
 
-public final class AuthCryptResponse {
+import io.netty.buffer.ByteBuf;
+
+import java.util.Arrays;
+
+public final class AuthCryptResponse implements BackendMessage<AuthCryptResponse.Data> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Data decode(ByteBuf src) {
+        var size = src.readableBytes();
+        var target = new byte[size];
+        for (var offset = size - 1; offset >= 0; offset--) {
+            target[offset] = src.readByte();
+        }
+        return new Data(target);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void encode(Data content, ByteBuf dst) {
+        var salt = content.salt();
+
+        for (var x = salt.length - 1; x >= 0; x--) {
+            dst.writeByte(salt[x]);
+        }
+    }
+
+    public static final class Data implements BackendContent {
+
+        private final byte[] salt;
+
+        public Data(byte[] salt) {
+            this.salt = salt;
+        }
+
+        public byte[] salt() {
+            return salt;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Data data = (Data) o;
+            return Arrays.equals(salt, data.salt);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(salt);
+        }
+
+        @Override
+        public String toString() {
+            return "Data{" +
+                    "salt=" + Arrays.toString(salt) +
+                    '}';
+        }
+    }
 
 }
---
+++ src/test/java/io/r2dbc/vertica/protocol/message/backend/AuthCryptResponseTest.java
@@ -0,0 +1,56 @@
+package io.r2dbc.vertica.protocol.message.backend;
+
+import io.netty.buffer.Unpooled;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class AuthCryptResponseTest {
+
+    private final AuthCryptResponse subject = new AuthCryptResponse();
+
+    @Nested
+    @DisplayName("decode(ByteBuf)")
+    class DecodeByteBuf {
+
+        @Test
+        void whenOk_thenReturn() {
+            var src = Unpooled.buffer().writeBytes(new byte[] {
+                (byte) 0x01, (byte) 0x02, (byte) 0x03,
+            });
+
+            Assertions.assertThat(subject.decode(src))
+                .isEqualTo(new AuthCryptResponse.Data(
+                    new byte[] { (byte) 0x03, (byte) 0x02, (byte) 0x01 }
+                ));
+        }
+
+    }
+
+    @Nested
+    @DisplayName("encode(T, ByteBuf)")
+    class EncodeTByteBuf {
+
+        @Test
+        void whenOk_thenReturn() {
+            var dst = Unpooled.buffer();
+
+            subject.encode(
+                new AuthCryptResponse.Data(new byte[] {
+                    (byte) 0x01, (byte) 0x02
+                }),
+                dst
+            );
+
+            Assertions.assertThat(dst)
+                .isEqualTo(
+                    Unpooled.buffer()
+                        .writeByte(0x02)
+                        .writeByte(0x01)
+                );
+        }
+
+    }
+
+}
